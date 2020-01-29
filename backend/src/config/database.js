f16e1c6158d4f503b7ffe0905a1e1a9b5bfc5eb9
RennanD <rplaykl@gmail.com>
2020-01-28 22:11:09 -0300

    feat: Configure sequelize

---
+++ backend/src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: '',
+  passwors: '',
+  database: '',
+  difine: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
