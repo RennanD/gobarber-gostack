f5cdc441026f3255ce5df7a39d66f2b531b83b6c
RennanD <rplaykl@gmail.com>
2020-01-29 22:14:32 -0300

    feat: Create authenticatde routes

--- backend/src/app/controllers/UserController.js
+++ backend/src/app/controllers/UserController.js
@@ -17,6 +17,10 @@ class UserController {
       provider,
     });
   }
+
+  async update(req, res) {
+    return res.json();
+  }
 }
 
 export default new UserController();
---
+++ backend/src/app/middlewares/auth.js
@@ -0,0 +1,27 @@
+import jwt from 'jsonwebtoken';
+
+// converte promisse para o padrão async await
+import { promisify } from 'util';
+
+import authConfig from '../../config/auth';
+
+export default async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).json({ error: 'Token not provided' });
+  }
+
+  // Divide em duas partes [ Bearer, 'token' ]
+  const [, token] = authHeader.split(' ');
+
+  try {
+    // constante retornada por padrão no jw
+    const decoded = await promisify(jwt.verify())(token, authConfig.secret);
+
+    // incluir id no req, para poder usar em outras requisições
+    req.useId = decoded.id;
+  } catch (err) {
+    return res.status(401).json({ error: 'Token invalid' });
+  }
+};
