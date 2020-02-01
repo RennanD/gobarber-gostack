350dacd38763376bbd30acac76ab339900f9d068
RennanD <rplaykl@gmail.com>
2020-02-01 18:11:56 -0300

    feat: Create notificatiom to provider

--- backend/src/app/controllers/AppointmentController.js
+++ backend/src/app/controllers/AppointmentController.js
@@ -1,9 +1,11 @@
 import * as Yup from 'yup';
-import { startOfHour, parseISO, isBefore } from 'date-fns';
+import { startOfHour, parseISO, isBefore, format } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
 
 import Appointment from '../models/Appointment';
 import User from '../models/User';
 import File from '../models/File';
+import Notification from '../schemas/Notification';
 
 class AppointmentController {
   async index(req, res) {
@@ -80,6 +82,16 @@ class AppointmentController {
         user_id: req.userId,
         provider_id,
         date: hourStart,
+      });
+
+      // Notify appointment provider
+      const user = await User.findByPk(req.userId);
+      const formatedDate = format(hourStart, "dd 'de' MMMM', às' HH:mm'h' ", {
+        locale: ptBR,
+      });
+      await Notification.create({
+        content: `Novo agendamento de ${user.name} para ${formatedDate}`,
+        user: provider_id,
       });
 
       return res.json(appointment);
---
+++ backend/src/app/schemas/Notification.js
@@ -0,0 +1,24 @@
+import { Schema, model } from 'mongoose';
+
+const NotificationSchema = new Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+    },
+    user: {
+      type: Number,
+      required: true,
+    },
+    read: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default model('Notification', NotificationSchema);
