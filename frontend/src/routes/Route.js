aff98c5274d6320c68acff7a4a5a22a04c370914
RennanD <rplaykl@gmail.com>
2020-02-22 01:51:30 -0300

    feat: Configure babel-plugin-import

--- frontend/src/routes/Route.js
+++ frontend/src/routes/Route.js
@@ -4,8 +4,8 @@ import { Route, Redirect } from 'react-router-dom';
 
 import PropTypes from 'prop-types';
 
-import AuthLayout from '../pages/_layouts/auth';
-import DefaultLayout from '../pages/_layouts/default';
+import AuthLayout from '~/pages/_layouts/auth';
+import DefaultLayout from '~/pages/_layouts/default';
 
 export default function CreateRoute({
   component: Component,
