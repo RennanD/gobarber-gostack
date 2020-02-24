63c9907d3e65148cc0a457943ee1ae910a0b881c
RennanD <rplaykl@gmail.com>
2020-02-24 12:33:22 -0300

    feat: Styled auth routes in web app

--- frontend/src/pages/SingIn/index.js
+++ frontend/src/pages/SingIn/index.js
@@ -1,7 +1,21 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 // import { Container } from './styles';
 
+import logo from '~/assets/logo.svg';
+
 export default function SingIn() {
-  return <div>Login</div>;
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+      <form>
+        <input type="email" placeholder="Seu e-mail" />
+        <input type="password" placeholder="Sua senha secreta" />
+
+        <button type="submit">Acessar</button>
+
+        <Link to="/register">Criar conta gratuita</Link>
+      </form>
+    </>
+  );
 }
--- frontend/src/pages/SingUp/index.js
+++ frontend/src/pages/SingUp/index.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-// import { Container } from './styles';
+import logo from '~/assets/logo.svg';
 
 export default function SingUp() {
-  return <div />;
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+      <form>
+        <input type="email" placeholder="Seu e-mail" />
+        <input type="text" placeholder="Nome completo" />
+        <input type="password" placeholder="Sua senha secreta" />
+
+        <button type="submit">Cadastrar-se</button>
+
+        <Link to="/">Já tenho login</Link>
+      </form>
+    </>
+  );
 }
